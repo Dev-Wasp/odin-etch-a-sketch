382efc416ccdd3c9b294e4a0443351b8ee16111c
Dev-Wasp <osadchyi.maksym.02@gmail.com>
2024-08-31 08:39:17 +0300

    Add functionality to change the grid dimensions
    Add html button and js code to allow users to changes
    dimensions of the grid.
    Modify css to account for the changing dimensions.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const gridContainer = document.querySelector(".container");
+const gridSizeBtn = document.querySelector(".grid-size-btn")
+
+const CONTAINER_WIDTH = 400;
 
 for (let i = 0; i < 16 * 16; i++) {
   const gridCell = document.createElement("div");
@@ -14,3 +17,71 @@ gridContainer.addEventListener("mouseover", e => {
 
   cell.style.backgroundColor = "#333";
 });
+
+gridSizeBtn.addEventListener("click", () => {
+  const numOfCells = getNumberOfCells();
+  createTheGrid(numOfCells);
+});
+
+function getNumberOfCells() {
+  let numOfCellsInRow;
+
+  do {
+    numOfCellsInRow = parseFloat(prompt("Enter the new number of cells in a row: "));
+
+    const isNaN = numOfCellsInRow === NaN;
+    const isFloat = !Number.isInteger(numOfCellsInRow);
+    const isOutOfBounds = numOfCellsInRow > 100 || numOfCellsInRow < 1;
+
+    if (isNaN || isFloat || isOutOfBounds) {
+      numOfCellsInRow = null;
+
+      alert("Please enter a positive whole number up to a 100");
+    }
+  } while (numOfCellsInRow === null);
+
+  const numOfCells = numOfCellsInRow**2;
+
+  return numOfCells;
+}
+
+function createTheGrid(numOfCells) {
+  const newCellWidth = calcNewCellWidth(numOfCells);
+
+  const newCells = genNewCells(newCellWidth, numOfCells);
+
+  removeOldCells();
+
+  addNewCells(newCells);
+}
+
+function calcNewCellWidth(numOfCells) {
+  const newCellWidth = Math.floor((CONTAINER_WIDTH / Math.sqrt(numOfCells)) * 10) / 10;
+
+  return newCellWidth;
+}
+
+function genNewCells(newCellWidth, numOfCells) {
+  let newCells = [];
+
+  for (let i = 0; i < numOfCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.width = `${newCellWidth}px`;
+    cell.style.height = `${newCellWidth}px`;
+
+    newCells.push(cell);
+  }
+
+  return newCells;
+}
+
+function removeOldCells() {
+  const oldCells = document.querySelectorAll(".cell");
+
+  oldCells.forEach(cell => cell.remove());
+}
+
+function addNewCells(newCells) {
+  newCells.forEach(cell => gridContainer.appendChild(cell));
+}
